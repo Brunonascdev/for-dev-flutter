d77e516eaa2c1120162db1e1e0f45edd1b6ede48
Bruno Nascimento <bruno.nascimento.extern@finn.auto>
2023-03-12 17:04:47 -0300

    remove @required from classes

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -1,10 +1,8 @@
-import 'package:meta/meta.dart';
-
 import '../entities/entities.dart';
 
 abstract class Authentication {
   Future<AccountEntity> auth({
-    @required String email,
-    @required String password,
+    required String email,
+    required String password,
   });
 }
