d2eee28c83d0ff09c8df62bd7bbf8b47099398f3
Bruno Nascimento <bruno.nascimento.extern@finn.auto>
2023-03-12 18:18:58 -0300

    move api details from domain class to data class

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -12,10 +12,32 @@ class RemoteAuthentication {
   });
 
   Future<void>? auth(AuthenticationParams params) async {
+    final body = RemoteAuthenticationParams.fromDomain(params).toJson();
     await httpClient.request(
       url: url,
       method: 'post',
-      body: params.toJson(),
+      body: body,
     );
   }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({
+    required this.email,
+    required this.password,
+  });
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams params) =>
+      RemoteAuthenticationParams(
+        email: params.email,
+        password: params.secret,
+      );
+
+  Map toJson() => {
+        'email': email,
+        'password': password,
+      };
 }
--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -12,9 +12,4 @@ class AuthenticationParams {
     required this.email,
     required this.secret,
   });
-
-  Map toJson() => {
-        'email': email,
-        'password': secret,
-      };
 }
